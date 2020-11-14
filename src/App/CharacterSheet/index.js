6761eb55b1865e6bc015b19829818b8cf413f7dc
Sam McCall <smccall@smccall.com>
2020-11-13 22:38:32 -0500

    Add initial character sheet info; remove Pure.css

--- src/App/CharacterSheet/index.js
+++ src/App/CharacterSheet/index.js
@@ -13,7 +13,50 @@ const testInfo = {
     "tough": 1,
     "weird": -1
   },
-  "moves": 0
+  "moves": [
+    {
+      "name": "Destiny’s Plaything",
+      "description": "At the beginning of each mystery, roll +Weird to see what is revealed about your immediate future. On a 10+, the Keeper will reveal a useful detail about the coming mystery. On a 7-9 you get a vague hint about it. On a miss, something bad is going to happen to you."
+    },
+    {
+      "name": "I’m Here For A Reason",
+      "description": "There’s something you are destined to do. Work out the details with the Keeper, based on your fate. You cannot die until it comes to pass. If you die in play, then you must spend a Luck point. You will then, somehow, recover or be returned to life. Once your task is done (or you use up all your Luck), all bets are off."
+    },
+    {
+      "name": "Invincible",
+      "description": "You always count as having 2-armour. This doesn’t stack with other protection."
+    }
+  ],
+  "weapons": [
+    {
+      "name": ".38 revolver",
+      "harm": 2,
+      "tags": [
+        {
+          "name": "Close",
+          "url": "http://motwapi.com/api/v1/weapon-tags/close"
+        },
+        {
+          "name": "Reload",
+          "url": "http://motwapi.com/api/v1/weapon-tags/reload"
+        },
+        {
+          "name": "Loud",
+          "url": "http://motwapi.com/api/v1/weapon-tags/loud"
+        }
+      ]
+    },
+    {
+      "name": "Big knife",
+      "harm": 1,
+      "tags": [
+        {
+          "name": "Hand",
+          "url": "http://motwapi.com/api/v1/weapon-tags/hand"
+        }
+      ]
+    }
+  ],
 }
 
 function CharacterSheet() {
@@ -25,6 +68,8 @@ function CharacterSheet() {
       <RowWithIcon label="Luck" type="luck" pointsLeft={testInfo.luck} />
       <RowWithExperienceBar label="Experience" experience={testInfo.experience} />
       <RowWithRatings label="Ratings" ratings={testInfo.ratings}/>
+      <RowWithMoves label="Moves" moves={testInfo.moves} />
+      <RowWithWeapons label="Weapons" weapons={testInfo.weapons}/>
     </div>
   );
 };
@@ -120,6 +165,60 @@ function RowWithRatings (props) {
     </Wrapper>
   );
 }
+
+function RowWithMoves (props) {
+  let movesList = [];
+  for (let move of props.moves) {
+    movesList.push(
+      <Row label={move.name} data={move.description} />
+    );
+  };
+
+  return (
+  <Wrapper>
+    <Row label={props.label} data={movesList} />
+  </Wrapper>
+  );
+};
+
+function RowWithWeapons (props) {
+  let weaponsList = [];
+  for (let weapon of props.weapons) {
+    let tags = []
+    for (let tag of weapon.tags) {
+      tags.push(tag.name);
+    };
+    weaponsList.push(
+      <tr>
+        <td>{weapon.name}</td>
+        <td>{weapon.harm}</td>
+        <td><p>{tags}</p></td>
+      </tr>
+    );
+  };
+
+  const weaponsData = (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Harm</th>
+          <th>Tags</th>
+        </tr>
+      </thead>
+      <tbody>
+        {weaponsList}
+      </tbody>
+    </table>
+  );
+
+  return (
+  <Wrapper>
+    <Row label={props.label} data={weaponsData} />
+  </Wrapper>
+  );
+};
+
 
 // SHARED STYLES
 
